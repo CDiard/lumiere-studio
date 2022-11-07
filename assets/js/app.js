ae738db7a00e4beb23562694d1d0296cf397c2c4
CDiard <91243460+CDiard@users.noreply.github.com>
2022-11-07 11:09:01 +0100

    Front - Menu : Modif selecteur

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    $('header .check-state-on').css('display', 'none');
+    $("header .toggle-input").prop("checked", false);
+    $('header .toggle-input').click(function() {
+        $('header .check-state-on').toggle();
+        $('header .check-state-off').toggle();
+    })
 });
