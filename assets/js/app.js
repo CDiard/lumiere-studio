9047693b147f4c1fc01169aade18dedddc00797e
Simon Guillet <simon.guillet@free.fr>
2022-11-15 17:52:18 +0100

    slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,49 @@
-$(document).ready(function() {
-    //Sélecteur on off (menu)
-    $('header .check-state-on').css('display', 'none');
-    $("header .toggle-input").prop("checked", false);
-    $('header .toggle-input').click(function() {
-        $('header .check-state-on').toggle();
-        $('header .check-state-off').toggle();
-    })
+$(document).ready(function () {
+	//Sélecteur on off (menu)
+	$("header .check-state-on").css("display", "none")
+	$("header .toggle-input").prop("checked", false)
+	$("header .toggle-input").click(function () {
+		$("header .check-state-on").toggle()
+		$("header .check-state-off").toggle()
+	})
 
-    //Iro.js
-    var colorPicker = new iro.ColorPicker('#pickerColor', {
-        width: 300,
-        color: "#ffffff",
-        layoutDirection: 'horizontal',
-        layout: [
-            {
-                component: iro.ui.Slider,
-                options: {
-                    sliderType: 'saturation',
-                    sliderSize: 35,
-                }
-            },
-            {
-                component: iro.ui.Wheel,
-                options: {
-                    sliderType: 'hue',
-                }
-            },
-            {
-                component: iro.ui.Slider,
-                options: {
-                    sliderType: 'value',
-                    sliderSize: 35,
-                }
-            },
-            {
-                component: iro.ui.Slider,
-                options: {
-                    sliderType: 'kelvin',
-                    layoutDirection: 'vertical',
-                    sliderSize: 35,
-                }
-            },
-        ]
-    });
+	//Iro.js
+	var colorPicker = new iro.ColorPicker("#pickerColor", {
+		width: 300,
+		color: "#ffffff",
+		layoutDirection: "horizontal",
+		layout: [
+			{
+				component: iro.ui.Slider,
+				options: {
+					sliderType: "saturation",
+					sliderSize: 35,
+				},
+			},
+			{
+				component: iro.ui.Wheel,
+				options: {
+					sliderType: "hue",
+				},
+			},
+			{
+				component: iro.ui.Slider,
+				options: {
+					sliderType: "value",
+					sliderSize: 35,
+				},
+			},
+			{
+				component: iro.ui.Slider,
+				options: {
+					sliderType: "kelvin",
+					layoutDirection: "vertical",
+					sliderSize: 35,
+				},
+			},
+		],
+	})
 
-    //Pour le responsive
-    //colorPicker.resize(200);
-});
+	//Pour le responsive
+	//colorPicker.resize(200);
+})
