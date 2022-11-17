0646c7e247a5e26f913e1de33dd0f044f2a85253
Simon Guillet <simon.guillet@free.fr>
2022-11-17 08:42:05 +0100

    fixed the toggle on off

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
 		arrows: false,
 		variableWidth: true,
 	})
+
+	//Sélecteur on off (menu)
+	$(".check-state-on").css("display", "none")
+	$(".toggle-input").prop("checked", false)
+	$(".toggle-input").click(function () {
+		$(".check-state-on").toggle()
+		$(".check-state-off").toggle()
+	})
 })
